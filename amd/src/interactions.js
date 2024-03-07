4a1def68e5c9522a37cb837fd83cdbb86ffac4b5
hector.arrechea <hector.arrechea@ct.uneatlantico.es>
2024-03-07 14:07:10 +0100

    feature: add interactions amd and create experiences view

---
+++ amd/src/interactions.js
@@ -0,0 +1,72 @@
+// En tu_plugin/amd/src/interactions.js
+define(['jquery', 'core/templates', 'core/notification'], function($, Templates, Notification) {
+    var likeCount = 256; // Valor inicial de likes quemado para el ejemplo.
+    // Actualizar la lista de comentarios, que podría provenir de una fuente de datos en el servidor.
+    var commentList = [{
+        name: "Usuario 1", comment: "Este es un comentario."
+    }, {
+        name: "Usuario 2", comment: "¡Otro comentario aquí!"
+    }];
+
+    /**
+     * Agrega un like al contenido.
+     * @return {void}
+     */
+    function addLike() {
+        likeCount++;
+        $('#likeCount').text(likeCount);
+        // Aquí se implementaría la llamada AJAX para actualizar los likes en el servidor.
+    }
+
+    /**
+     * Agrega un dislike al contenido.
+     * @return {void}
+     */
+    function addDislike() {
+        // La lógica de dislike se implementaría aquí. No hay una actualización visible en la UI para el número de dislikes.
+    }
+
+    /**
+     * Muestra u oculta la sección de comentarios.
+     * @return {void}
+     */
+    function toggleComments() {
+        $('#commentsCollapse').collapse('toggle');
+    }
+
+    /**
+     * Envía un comentario al servidor.
+     * @return {void}
+     */
+    function sendComment() {
+        var commentText = $('#commentText').val().trim();
+        if (commentText) {
+            commentList.push({name: "Usuario Nuevo", comment: commentText}); // Agregar el comentario al array.
+            updateCommentsList(); // Actualizar la lista de comentarios en la UI.
+            $('#commentText').val(''); // Limpiar el campo de texto.
+            // Aquí se implementaría la llamada AJAX para enviar el comentario al servidor.
+        }
+    }
+
+    /**
+     * Actualiza la lista de comentarios en la UI.
+     * @return {void}
+     */
+    function updateCommentsList() {
+        Templates.render('local_dta/experience/comment', {comments: commentList}).then(function(html) {
+            $('#commentsList').html(html);
+        }).fail(Notification.exception);
+    }
+
+    // Exponer las funciones al objeto window para hacerlas accesibles globalmente.
+    window.addLike = addLike;
+    window.addDislike = addDislike;
+    window.toggleComments = toggleComments;
+    window.sendComment = sendComment;
+
+    return {
+        init: function() {
+            updateCommentsList(); // Llamar a esta función para cargar los comentarios cuando se inicializa la página.
+        }
+    };
+});
---
+++ pages/experience/view.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * community page
+ *
+ * @package   local_dta
+ * @copyright 2024 ADSDR-FUNIBER Scepter Team
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use local_dta\Experience;
+
+require_once(__DIR__ . '/../../../../config.php');
+require_once(__DIR__ . './../../classes/experiences.php');
+require_once(__DIR__ . './../../classes/ourcases.php');
+
+
+require_login();
+
+global $CFG, $PAGE, $OUTPUT , $USER;
+
+
+// Get the experience
+$id = required_param('id', PARAM_INT);
+if(!$experience = Experience::getExperience($id)) {
+    throw new moodle_exception('invalidexperience', 'local_dta');
+}
+
+// Get the user and the user picture
+$user = get_complete_user_data("id", $experience->user);
+$user_picture = new user_picture($user);
+$user_picture->size = 101;
+
+
+// Seting the page url and context
+$PAGE->set_url(new moodle_url('/local/dta/experience/view.php'));
+$PAGE->set_context(context_system::instance());
+$PAGE->requires->js_call_amd('local_dta/interactions', 'init');
+
+echo $OUTPUT->header();
+
+$templateContext = [
+    'experience' => [
+        'id' => $experience->id,
+        'title' => $experience->title,
+        'description' => $experience->description,
+        'date' => $experience->date,
+        'lang' => $experience->lang,
+        'picture_url' => $experience->pictureurl,
+        'visible' => $experience->visible,
+        'comments' => []
+    ],
+    'user' => [
+        'id' => $user->id,
+        'name' => $user->firstname . " " . $user->lastname,
+        'email' => $user->email,
+        'picture_url' => $user_picture->get_url($PAGE)->__toString(),
+    ],
+    'is_admin' => is_siteadmin($USER),
+    'userprofile_url' => $CFG->wwwroot . "/user/profile.php?id=" . $user->id,
+    'delete_url' => $CFG->wwwroot . "/local/dta/pages/experience/delete.php?id=",
+    'edit_url' => $CFG->wwwroot . "/local/dta/pages/experience/edit.php?id=",
+];
+
+echo $OUTPUT->render_from_template('local_dta/experience/experience', $templateContext);
+
+echo $OUTPUT->footer();

6d34b3babbb4800cac2fc1a702e45e5018816165
Franklin Alejando López Ramírez <franklin.lopez@ct.uneatlantico.es>
2024-03-26 17:44:49 +0100

    Fix bug in login functionality

--- db/upgrade.php
+++ db/upgrade.php
@@ -16,23 +16,153 @@
 /**
  * Upgrade script for local_dta
  */
-// function xmldb_local_dta_upgrade($oldversion) {
-//     global $DB;
-//     $dbman = $DB->get_manager(); // Loads ddl manager and xmldb classes.
-//    if ($oldversion < [nueva versión]) {
-//     // Define table
-//     $table = new xmldb_table('new_table_name');
-//     // Adding fields to table
-//     $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-//     // Añade más campos según sea necesario
-//     // Adding keys to table
-//     $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
-//     // Conditionally launch create table for new_table_name
-//     if (!$dbman->table_exists($table)) {
-//         $dbman->create_table($table);
-//     }
-//     // Actualización de la versión del plugin y registro del punto de actualización
-//     upgrade_plugin_savepoint(true, [nueva versión], 'local', 'dta');
-// }
-//     return true;
-// }
+
+function xmldb_local_dta_upgrade($oldversion)
+{
+    global $DB;
+
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2024031307) {
+        // Define table digital_cases_likes to be created.
+        $table = new xmldb_table('digital_case_likes');
+
+        // Adding fields to table digital_cases_likes.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'sequence' => true]);
+        $table->add_field('caseid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('reactiontype', XMLDB_TYPE_INTEGER, '1', ['unsigned' => false, 'notnull' => false]);
+
+        // Adding keys to table digital_cases_likes.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('caseid', XMLDB_KEY_FOREIGN, ['caseid'], 'digital_ourcases', ['id']);
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']);
+
+        // Conditionally launch create table for digital_cases_likes.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define table digital_cases_comments to be created.
+        $table = new xmldb_table('digital_case_comments');
+
+        // Adding fields to table digital_cases_comments.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'sequence' => true]);
+        $table->add_field('caseid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('comment', XMLDB_TYPE_TEXT, null, ['notnull' => true]);
+
+        // Adding keys to table digital_cases_comments.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('caseid', XMLDB_KEY_FOREIGN, ['caseid'], 'digital_ourcases', ['id']);
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']);
+
+        // Conditionally launch create table for digital_cases_comments.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // DTA savepoint reached.
+        upgrade_plugin_savepoint(true, 2024031306, 'local', 'dta');
+    }
+
+
+    if ($oldversion < 2024032001) {
+
+        // Define table digital_cases_comments to be created.
+        $table = new xmldb_table('digital_experience_report');
+
+        // Adding fields to table digital_cases_likes.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'sequence' => true]);
+        $table->add_field('experienceid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+
+        // Adding keys to table digital_cases_likes.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('experienceid', XMLDB_KEY_FOREIGN, ['experienceid'], 'digital_experiences', ['id']);
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']);
+
+        // Conditionally launch create table for digital_cases_comments.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define table digital_cases_comments to be created.
+        $table = new xmldb_table('digital_case_report');
+
+        // Adding fields to table digital_cases_likes.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'sequence' => true]);
+        $table->add_field('caseid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+
+        // Adding keys to table digital_cases_likes.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('caseid', XMLDB_KEY_FOREIGN, ['caseid'], 'digital_ourcases', ['id']);
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']);
+
+        // Conditionally launch create table for digital_cases_comments.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // DTA savepoint reached.
+        upgrade_plugin_savepoint(true, 2024032001, 'local', 'dta');
+    }
+    
+    if ($oldversion < 2024032600) {
+
+        // Table for storing reflections
+        $table = new xmldb_table('digital_reflection');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true, 'sequence' => true]);
+        $table->add_field('experienceid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => false]);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('timecreated', XMLDB_TYPE_DATETIME, null, ['notnull' => true]);
+        $table->add_field('timemodified', XMLDB_TYPE_DATETIME, null, ['notnull' => true]);
+        $table->add_field('status', XMLDB_TYPE_INTEGER, '1', ['notnull' => true, 'default' => 0]);
+        $table->add_field('visible', XMLDB_TYPE_INTEGER, '1', ['notnull' => true, 'default' => 0]);
+    
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('experienceid', XMLDB_KEY_FOREIGN, ['experienceid'], 'digital_experiences', ['id']);
+        $table->add_key('userid', XMLDB_KEY_FOREIGN, ['userid'], 'user', ['id']); // Ensure 'user' table is correctly referenced
+    
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+    
+        // Table for storing reflection sections
+        $table = new xmldb_table('digital_refl_sections');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true, 'sequence' => true]);
+        $table->add_field('reflectionid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('groupid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('sequence', XMLDB_TYPE_INTEGER, '11', ['notnull' => true]);
+        $table->add_field('sectiontype', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('contentid', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true]);
+        $table->add_field('timecreated', XMLDB_TYPE_DATETIME, null, ['notnull' => true]);
+        $table->add_field('timemodified', XMLDB_TYPE_DATETIME, null, ['notnull' => true]);
+    
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('reflectionid', XMLDB_KEY_FOREIGN, ['reflectionid'], 'digital_reflection', ['id']);
+    
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+    
+        // Table for storing text section content
+        $table = new xmldb_table('digital_refl_sec_text');
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '11', ['unsigned' => true, 'notnull' => true, 'sequence' => true]);
+        $table->add_field('content', XMLDB_TYPE_TEXT, null, ['notnull' => true]);
+    
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+    
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+    
+        // Savepoint reached
+        upgrade_plugin_savepoint(true, 2024032600, 'local', 'dta');
+    }
+
+
+
+    return true;
+}
---
+++ pages/cases/manage.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * ourcases manage page
+ *
+ * @package   local_dta
+ * @copyright 2024 ADSDR-FUNIBER Scepter Team
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require_once(__DIR__ . '/../../../../config.php');
+require_once(__DIR__ . './../../classes/experience.php');
+require_once(__DIR__ . './../../classes/ourcases.php');
+require_once(__DIR__ . './../../classes/tiny_editor_handler.php');
+
+use local_dta\Experience;
+use local_dta\OurCases;
+use local_dta\tiny_editor_handler;
+
+require_login();
+
+global $CFG, $PAGE, $OUTPUT, $USER;
+
+$experienceid = optional_param('id', 0, PARAM_INT);
+$case = optional_param('caseid', 0, PARAM_INT);
+$case_title = optional_param('  ', 0, PARAM_RAW);
+
+$strings = get_strings(['ourcases_header', 'ourcases_title'], "local_dta");
+
+$PAGE->set_url(new moodle_url('/local/dta/pages/cases/manage.php', ['id' => $experienceid]));
+$PAGE->set_context(context_system::instance());
+$PAGE->set_title($strings->ourcases_title);
+$PAGE->requires->js_call_amd(
+    'local_dta/ourcases/manage',
+    'init',
+    array(
+        'url_view' => $CFG->wwwroot . '/local/dta/pages/cases/repository.php', 
+    )
+);
+
+echo $OUTPUT->header();
+
+(new tiny_editor_handler)->get_config_editor(['maxfiles' =>1]);
+
+
+if ($experienceid) {
+    // IF EXPERIENCE EXISTS
+    if (!$experience = Experience::get_experience($experienceid)) {
+        throw new moodle_exception('invalidcases', 'local_dta');
+    }
+
+    if (!$ourcase = OurCases::get_case_by_experience($experienceid)) {
+        $ourcase = OurCases::add_with_experience($experienceid, date("Y-m-d H:i:s"), $USER->id);
+    }
+
+    $sections = array_values(OurCases::get_sections_text($ourcase->id));
+
+    if (!$section_header = OurCases::get_section_header($ourcase->id)) {
+        throw new moodle_exception('invalidcasessection', 'local_dta');
+    }
+
+    $templateContext = [
+        'experience' => $experience,
+        'sectionheader' => $section_header,
+        'sections' => $sections,
+        'ourcase' => $ourcase,
+    ];
+
+    echo $OUTPUT->render_from_template('local_dta/cases/manage-with-experience', $templateContext);
+} elseif ($case) {
+    // IF CASE EXISTS
+    if (!$ourcase = OurCases::get_case($case)) {
+        throw new moodle_exception('invalidcases', 'local_dta');
+    };
+    if ($case_title) $section_header->title = $case_title;
+
+    $sections = array_values(OurCases::get_sections_text($ourcase->id));
+    $section_header = OurCases::get_section_header($ourcase->id);
+    $templateContext = [
+        'sectionheader' => $section_header,
+        'sections' => $sections,
+        'ourcase' => $ourcase,
+    ];
+
+
+    echo $OUTPUT->render_from_template('local_dta/cases/manage-without-experience', $templateContext);
+} else {
+    // IF NO EXPERIENCE OR CASE
+    $ourcase = OurCases::add_without_experience(date("Y-m-d H:i:s"), $USER->id);
+
+    if (!$section_header = OurCases::get_section_header($ourcase->id)) {
+        throw new moodle_exception('invalidcasessection', 'local_dta');
+    }
+    if ($case_title) $section_header->title = $case_title;
+
+    $sections = array_values(OurCases::get_sections_text($ourcase->id));
+    $section_header->title = format_text($section_header->title, FORMAT_HTML, ['filter' => true]);
+    $templateContext = [
+        'sectionheader' => $section_header,
+        'sections' => $sections,
+        'ourcase' => $ourcase,
+    ];
+
+    echo $OUTPUT->render_from_template('local_dta/cases/manage-without-experience', $templateContext);
+}
+
+
+
+echo $OUTPUT->footer();

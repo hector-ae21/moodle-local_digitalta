0806e98d4bb05cbb86c0da5597acabb28cc29429
Eduardo Estrada <me@e2rd0.com>
2024-05-03 14:06:12 +0200

    feature / change from add resources modal to import resources modal

--- pages/repository/index.php
+++ pages/repository/index.php
@@ -25,7 +25,7 @@ $strings = get_strings(['repository_header', 'repository_title'], "local_dta");
 $PAGE->set_url(new moodle_url('/local/dta/pages/repository/index.php'));
 $PAGE->set_context(context_system::instance());
 $PAGE->set_title($strings->repository_title);
-$PAGE->requires->js_call_amd('local_dta/resources/manage_resources', 'init');
+$PAGE->requires->js_call_amd('local_dta/resources/manage_resources', 'init', array(false));
 
 echo $OUTPUT->header();
 

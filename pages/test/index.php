c4c64834333a151728d056e3d65bb7953f26450d
Hector Arrechea <hector.arrechea@ct.uneatlantico.es>
2024-05-09 14:21:04 +0300

    refactor: Update index.php and index.mustache templates for test page

--- pages/test/index.php
+++ pages/test/index.php
@@ -36,7 +36,41 @@ $user_list = [
   ],
 ];
 
+$data = '{
+  "days": [
+    {
+      "day": "Monday",
+      "times": [
+        {"start": "09:00", "end": "10:00"},
+        {"start": "10:30", "end": "12:00"},
+        {"start": "16:00", "end": "17:00"}
+      ]
+    },
+    {
+      "day": "Tuesday",
+      "times": [
+        {"start": "09:00", "end": "14:00"}
+      ]
+    },
+    {
+      "day": "Wednesday",
+      "times": [
+        {"start": "09:00", "end": "14:00"}
+      ]
+    },
+    {
+      "day": "Thursday",
+      "times": [
+        {"start": "09:00", "end": "14:00"}
+      ]
+    }
+  ]
+}
+';
 
-echo $OUTPUT->render_from_template('local_dta/test/index', $user_list);
+$data = json_decode($data);
+
+
+echo $OUTPUT->render_from_template('local_dta/test/index', $data);
 
 echo $OUTPUT->footer();

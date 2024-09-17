b959f4c97078520a51f201122bf9a511c664d682
Hector Arrechea <hector.arrechea@ct.uneatlantico.es>
2024-09-17 15:50:25 +0200

    fix: add proccess file to all tiny instances

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024091701;
+$plugin->version   = 2024091702;
 $plugin->requires  = 2022112800;
 $plugin->component = 'local_digitalta';
 $plugin->maturity  = MATURITY_STABLE;

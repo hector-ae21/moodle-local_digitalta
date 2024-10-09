9a46dad0961b451dc5529ecd5c7407e5cf69e81b
Hector Arrechea <hectorlazaroarrechea@gmail.com>
2024-10-09 11:54:53 +0200

    Update view.php

--- pages/experiences/view.php
+++ pages/experiences/view.php
@@ -175,7 +175,7 @@ $template_context = [
     'mentoringrequest' => $mentoring_tutor_request,
     'mentors_from_requests' => $mentors_from_requests,
     'modschedulerurl' => $CFG->wwwroot . '/mod/scheduler/view.php?id=' . get_config('local_digitalta', 'schedulerinstance')
-];);
+];
 
 $experience_chat = Chat::get_chat_room_by_experience($id);
 if ($experience_chat) {

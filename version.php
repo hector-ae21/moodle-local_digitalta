c65d66a814ec783cd2c045585ae7ee7e7bbd84f1
Eduardo Estrada <me@e2rd0.com>
2024-11-11 10:56:57 +0100

    hotfix / tags error

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024110700;
+$plugin->version   = 2024111100;
 $plugin->requires  = 2022112800;
 $plugin->component = 'local_digitalta';
 $plugin->maturity  = MATURITY_STABLE;
